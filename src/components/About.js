406b5a9927d7b8da9d032f22678551c7ddc11d23
Michael Moreno <mmoreno2222@gmail.com>
2021-01-26 13:40:33 -0800

    initial commit + components

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <img src="./assets/images/MM_Seattle.jpg" alt="Photo of Michael Moreno" />
+      <p>
+        I am a front-end web developer with a Bachelors in Psychology from the
+        University of San Francisco with additional study of Java programming
+        and web design at East Los Angeles College. I have recently completed a
+        certificate in full-stack web development from the UCLA Extension Coding
+        Bootcamp with newly developed skills in CSS, JavaScript, React, and
+        Responsive Web Design.
+      </p>
+      <p>
+        I am adaptable, self-motivated, and have an eye for detail which I use
+        to create and debug a variety of web-based projects. With my psychology
+        background, I take particular interest in creating websites that adhere
+        to accessibility standards while still creating an appealing client
+        experience. I am eager to continue acquiring new skills, learn from
+        future mentors and team members alike, and leverage my passion for
+        individuals and technical web development skills to develop better
+        experiences on the web.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/Hero.css";
+
+export default function Hero() {
+  return (
+    <div>
+      <h1 className="heroText">Hello</h1>
+      <h1 className="heroText">My Name is Michael</h1>
+    </div>
+  );
+}
