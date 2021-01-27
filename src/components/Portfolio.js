98f8c9e8bac8d851d2889263dd5b6062d448ecc3
Michael Moreno <mmoreno2222@gmail.com>
2021-01-27 14:30:19 -0800

    portfolio updated

---
+++ src/components/Portfolio.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Portfolio() {
+  return (
+    <>
+      <div>
+        <img src="./assets/images/Vibee.png" alt="Vibee home page" />
+        <h3>Vibee</h3>
+        <p>React-based Music-themed Social Media Website</p>
+        <p>React, MongoDB, Firebase Auhentication, Socket.io, Axios/API, Javascript, Bootstrap</p>
+      </div>
+      <div>
+        <img src="./assets/images/Buzzkill1.png" alt="Buzzkill logo" />
+        <h3>Buzzkill</h3>
+        <p>MySQL/Sequelize-based database themed around COVID-19 health regulation.</p>
+        <p>SQL, Bootstrap, API, Javascript, Bootstrap</p>
+      </div>
+      <div>
+        <img src="./assets/images/pubProject1.png" alt="Ultimate Pub Crawl home page" />
+        <h3>Ultimate Pub Crawl</h3>
+        <p>Javascript-based Pub Crawl Generator.</p>
+        <p>Javascript, HTML/CSS, API, Materialize</p>
+      </div>
+      <div>
+        <img src="./assets/images/DailyPlanner1.png" alt="Daily Planner website" />
+        <h3></h3>
+        <p></p>
+        <p></p>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <h3></h3>
+        <p></p>
+        <p></p>
+      </div>
+    </>
+  );
+}
