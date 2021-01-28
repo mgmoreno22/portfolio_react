3e94549232fbb2b074aaaa3e76485d211112b353
Michael Moreno <mmoreno2222@gmail.com>
2021-01-27 17:14:21 -0800

    removing first contact form backend

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function Contact() {
-  const [status, setStatus] = useState("Submit");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
-  };
 
   return (
     <form onSubmit={handleSubmit}>
